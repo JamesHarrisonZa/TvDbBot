e3a6498c6141b37886997b2f2209399f58939b76
James Harrison <JamesH@SAHL.COM>
2017-05-19 10:51:28 +0200

    Updated text

--- src/Message/Help.ts
+++ src/Message/Help.ts
@@ -8,11 +8,11 @@ export class Help {
 
 	private GetPhrases(): string[] {
 		return [
-			'Hi ! Its a pleasure to meet you! My creator and god hasnt given me a proper name yet. \
-			I have watched all the TV and Anime in the world and can let you know when the newest episodes of your favourite series are coming out. \
-			And hopefully soon, be able to send you a reminder when they do. \
-			Why dont you try ask me some questions! \
-			As an example why dont you try ask: When is the next Game of thrones?'
+			'Hi ! Its a pleasure to meet you! My creator and god hasnt given me a proper name yet. ' +
+			'I have watched all the TV and Anime in the world and can let you know when the newest episodes of your favourite series are coming out. ' +
+			'And hopefully soon, be able to send you a reminder when they do. ' +
+			'Why dont you try ask me some questions! ' +
+			'As an example why dont you try ask: When is the next Game of thrones?'
 		];
 	}
 }
