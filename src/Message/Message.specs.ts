9536b63954633b129263564c5626c4c25991ea0e
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-07-27 14:29:51 +0200

    Changed test structure to sut, actual, expected

--- src/Message/Message.specs.ts
+++ src/Message/Message.specs.ts
@@ -6,55 +6,50 @@ import { SeriesOverApology } from './SeriesOverApology';
 
 describe('Greeting', () => {
 
-	const greeting = new Greeting();
-
 	it('Should have collection of phrases', () => {
-
-		expect(greeting.phrases).not.toBe(null);
-		expect(greeting.phrases.length > 0).toBe(true);
+		const sut = new Greeting();
+		expect(sut.phrases).toBeDefined();
+		expect(sut.phrases.length).toBeGreaterThan(0);
 	});
 });
 
 describe('Apology', () => {
 
-	const apology = new Apology();
 
 	it('Should have collection of phrases', () => {
 
-		expect(apology.phrases).not.toBe(null);
-		expect(apology.phrases.length > 0).toBe(true);
+		const sut = new Apology();
+		expect(sut.phrases).toBeDefined();
+		expect(sut.phrases.length).toBeGreaterThan(0);
 	});
 });
 
 describe('Help', () => {
 
-	const help = new Help();
-
 	it('Should have collection of phrases', () => {
 
-		expect(help.phrases).not.toBe(null);
-		expect(help.phrases.length > 0).toBe(true);
+		const sut = new Help();
+		expect(sut.phrases).toBeDefined();
+		expect(sut.phrases.length).toBeGreaterThan(0);
 	});
 });
 
 describe('NoResultsApology', () => {
 
-	const noResultsApology = new NoResultsApology();
-
 	it('Should have collection of phrases', () => {
 
-		expect(noResultsApology.phrases).not.toBe(null);
-		expect(noResultsApology.phrases.length > 0).toBe(true);
+		const sut = new NoResultsApology();
+		expect(sut.phrases).toBeDefined();
+		expect(sut.phrases.length).toBeGreaterThan(0);
 	});
 });
 
 describe('SeriesOverApology', () => {
 
-	const seriesOverApology = new SeriesOverApology();
-
 	it('Should have collection of phrases', () => {
 
-		expect(seriesOverApology.phrases).not.toBe(null);
-		expect(seriesOverApology.phrases.length > 0).toBe(true);
+		const sut = new SeriesOverApology();
+		expect(sut.phrases).toBeDefined();
+		expect(sut.phrases.length).toBeGreaterThan(0);
 	});
 });
